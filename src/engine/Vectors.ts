35db982eecf3ddfa3c1cd935f12f222f63495f7c
Corne97 <you@example.com>
2023-05-08 10:40:29 +0200

    Made an vector class with constructos and vectors

---
+++ src/engine/Vectors.ts
@@ -0,0 +1,20 @@
+import { Engine } from "./Engine";
+
+class Vector2
+{
+    components: any;
+    static add: any;
+    public argument: number;
+    
+    // constructor for components and arguments 
+    constructor(components: any,  argument:number   )
+    {
+        this.components = components
+        this.argument = argument
+    }
+
+}
+
+const direction2d = new Vector2(10,20);
+const direction3d = new Vector2(20,49);
+
