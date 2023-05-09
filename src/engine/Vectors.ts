4a9547d90a7d052c5098801b4b6597b33a079e57
Corne97 <you@example.com>
2023-05-09 09:00:41 +0200

    updated vector class, added all the operators

--- src/engine/Vectors.ts
+++ src/engine/Vectors.ts
@@ -1,27 +1,41 @@
 import { Engine } from "./Engine";
 
-class Vector2
+class Vector
 {
     public x : number;
     public y : number;
-    public argument: number;
-    static add: any;
-    static subtract: any;
+   
 
 
     
     // constructor for components and arguments 
-    constructor( argument:number, x:number, y:number )
+    constructor(  x:number, y:number )
     {
         this.x = x
         this.y = y
-        this.argument = argument
+       
     }
+
+// add operator (+)
+add (vector:Vector): Vector{
+return new Vector(this.x+ vector.x, this.y +vector.y); 
+}
+// substraction operator(-)
+subtract (vector:Vector): Vector{
+    return new Vector(this.x- vector.x, this.y -vector.y); 
+    }
+
+// multiply operator(*)
+    multiply (vector:Vector): Vector{
+        return new Vector(this.x* vector.x, this.y *vector.y); 
+        }
+// division operator(/)
+        divide (vector:Vector): Vector{
+            return new Vector(this.x/ vector.x, this.y /vector.y); 
+            }
 
 }
 
-const direction2d = new Vector2(10,20);
-const direction3d = new Vector2(20,49);
 
 
 
